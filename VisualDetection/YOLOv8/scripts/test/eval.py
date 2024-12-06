f2bae40142af84bfd58832f83016b7ece0879917
4090 <2831185361@qq.com>
2024-12-06 15:37:11 +0800

    更新评估脚本以支持测试集评估，修改训练数据路径，调整训练轮次为100，新增多个训练参数配置文件及训练结果CSV文件。

--- VisualDetection/YOLOv8/scripts/test/eval.py
+++ VisualDetection/YOLOv8/scripts/test/eval.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     #加载训练好的模型
     model = YOLO('D:/detection/UAV_Detection/VisualDetection/YOLOv8/runs/detect/train2/weights/best.pt')
     # 对验证集进行评估
-    metrics = model.val(data = 'D:/detection/UAV_Detection/VisualDetection/YOLOv8/data.yaml')
+    metrics = model.val(data = 'D:/detection/UAV_Detection/VisualDetection/YOLOv8/data.yaml', split='test')
--- VisualDetection/YOLOv8/scripts/train/train.py
+++ VisualDetection/YOLOv8/scripts/train/train.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     # 初始训练
     model = YOLO("yolov8.yaml").load("yolov8n.pt")# 加载预训练模型，如果本地没有会自动下载
-    results = model.train(data="data.yaml", epochs=500, imgsz=640, batch=32, workers=16)
+    results = model.train(data="data.yaml", epochs=100, imgsz=640, batch=32, workers=16)
