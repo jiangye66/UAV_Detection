88d85fa61a52b1160f03cdddd8b97e5fdd6a3bd3
jiangye66 <2831185361@qq.com>
2024-12-06 15:38:22 +0800

    更新模型路径和验证集路径，调整训练轮数至100，新增测试集路径，添加详细评估指标输出，新增多个训练配置文件及结果文件。

--- VisualDetection/YOLOv8/script/test/eval.py
+++ VisualDetection/YOLOv8/script/test/eval.py
@@ -5,6 +5,17 @@ matplotlib.use( "TkAgg")
 
 if __name__ == '__main__':
     #加载训练好的模型
-    model = YOLO('D:/detection/UAV_Detection/VisualDetection/YOLOv8/runs/detect/train2/weights/best.pt')
+    model = YOLO('D:/code/UAV_Detection/VisualDetection/YOLOv8/script/train/runs/best.pt')
     # 对验证集进行评估
-    metrics = model.val(data = 'D:/detection/UAV_Detection/VisualDetection/YOLOv8/data.yaml')
+    results = model.val(data = 'D:/code/UAV_Detection/VisualDetection/YOLOv8/script/train/data.yaml', split='test')
+
+# 打印主要评估指标
+print(f"mAP50: {results.metrics['map50']:.4f}")  # 平均精度 (IoU=0.5)
+print(f"mAP50-95: {results.metrics['map']:.4f}")  # 平均精度 (IoU=0.5:0.95)
+print(f"Precision: {results.metrics['precision']:.4f}")  # 精确率
+print(f"Recall: {results.metrics['recall']:.4f}")  # 召回率
+
+# 如果你想打印所有指标
+print("\n详细评估指标:")
+for key, value in results.metrics.items():
+    print(f"{key}: {value:.4f}")
